0663f2c3cb015a9728a89c6462b58b06dacf55bd
Skyrim1ee <ljw511288736@163.com>
2023-04-19 19:48:08 +0800

    add Test

--- src/ArrayDemo/ArrayDemoTest/ArraysTest04.java
+++ src/ArrayDemo/ArrayDemoTest/ArraysTest04.java
@@ -2,6 +2,21 @@ package ArrayDemo.ArrayDemoTest;
 
 public class ArraysTest04 {
     public static void main(String[] args) {
-        
+        System.out.println(methodNumber(7));
+        System.out.println(methodNumber(20));
+        System.out.println(methodNumber(19));
+        System.out.println(methodNumber(18));
+    }
+
+    public static int methodNumber(int step) {
+        if (step == 0) {
+            return 0;
+        } else if (step == 1) {
+            return 1;
+        } else if (step == 2) {
+            return 2;
+        } else {
+            return methodNumber(step - 1) + methodNumber(step - 2);
+        }
     }
 }
